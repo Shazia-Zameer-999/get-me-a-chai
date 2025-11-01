91742048cfb646673ed0ab6755f1ddcdc3d9aceb
Daten by SZ <datendiva7867@gmail.com>
2025-11-01 20:16:11 +0530

    p4

--- models/Payment.js
+++ models/Payment.js
@@ -2,11 +2,15 @@ import mongoose from 'mongoose';
 const { Schema, model, models } = mongoose;
 
 const PaymentSchema = new Schema({
-    name: { type: String , required: true },
+    name: { type: String, required: true },
     to_username: { type: String, required: true },
     oid: { type: String, required: true },
-    message: { type: String , required: true },
-    amount: { type: Number, required: true },
+    message: { type: String, required: true },
+    amount: {
+        type: Number,
+        required: true,
+        set: (v) => v / 100
+    },
     done: { type: Boolean, default: false },
 }, { timestamps: true });
 
